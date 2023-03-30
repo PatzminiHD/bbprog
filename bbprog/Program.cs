16978562f0747b092d6698ad143a65a9286c6605
PatzminiHD <91912090+PatzminiHD@users.noreply.github.com>
2023-03-30 20:04:43 +0200

    Initial Commit

---
+++ bbprog/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Diagnostics;
+using static System.Console;
+
+namespace bbprog
+{
+    static class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
